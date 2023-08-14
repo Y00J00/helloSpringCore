fded8cffdb20e7591c02fa8660d0bbebdb7dfae6
Y00J00 <syeunnn@gmail.com>
2023-08-14 14:38:27 +0900

    할인 정책 적용

---
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10; // 10퍼센트 할인
+    @Override
+    public int discount(Member member, int price) { // 할인 금액 return 
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+    
+}
